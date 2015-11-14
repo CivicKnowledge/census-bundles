2f91172af967f329a8036e7b814741d63e2ff36e
Eric Busboom <eric@clarinova.com>
2015-11-13 22:57:29 -0800

    .

---
+++ census.gov/acs-p1ye2010/bundle.py
@@ -0,0 +1,7 @@
+ # -*- coding: utf-8 -*-
+import ambry.bundle 
+
+from censuslib import ACS2010Bundle, ACS09TableRowGenerator as TableRowGenerator
+
+class Bundle(ACS2010Bundle):
+    pass
---
+++ census.gov/acs-p3ye2009/bundle.py
@@ -0,0 +1,7 @@
+ # -*- coding: utf-8 -*-
+import ambry.bundle 
+
+from censuslib import ACS2009Bundle
+
+class Bundle(ACS2009Bundle):
+    pass
