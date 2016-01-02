3abe5c895e3190218ee42211dcd195ff3ae9b9d4
Eric Busboom <eric@clarinova.com>
2016-01-01 20:33:55 -0800

    .

--- census.gov/acs-p5ye2009/bundle.py
+++ census.gov/acs-p5ye2009/bundle.py
@@ -1,36 +1,8 @@
  # -*- coding: utf-8 -*-
 import ambry.bundle 
-from ambry.util import memoize
 
+from censuslib import ACS2009Bundle
 from censuslib import ACS09TableRowGenerator as TableRowGenerator
-from censuslib import MakeTableMixin, MakeSourcesMixin, JamValueMixin
-from censuslib import JoinGeofileMixin, ACS2009Bundle
 
 class Bundle(ACS2009Bundle):
-
-   
-    ##
-    ##
-    ##
- 
-
-    def post_build(self, phase='build'):
-        """After the build, update the configuration with the time required for
-        the build, then save the schema back to the tables, if it was revised
-        during the build."""
-
-        try:
-            self.build_post_unify_partitions()
-        except Exception:
-            self.set_error_state()
-            self.commit()
-            raise
-
-        return True
-       
-    def post_everything(self):
-        self.library.search.index_bundle(self, force=True)
-
-        self.state = phase + '_done'
-
-        self.log("---- Finished Build ---- ")
+    pass
