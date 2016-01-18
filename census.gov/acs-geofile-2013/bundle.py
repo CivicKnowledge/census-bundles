0ac97db7af9d2d069086c71e7eb22909964c0940
Eric Busboom <eric@clarinova.com>
2016-01-17 21:35:36 -0800

    Added a lot of new census bundles

---
+++ census.gov/acs-geofile-2013/bundle.py
@@ -0,0 +1,16 @@
+"""
+
+See the header for censuslib.geofile for instructions, 
+or run 'bambry exec geobule_doc', after setting the base class to
+GeofileBundle
+
+"""
+from censuslib.geofile import GeofileBundle
+
+
+
+class Bundle(GeofileBundle):
+    
+    year = 2013
+    
+      
