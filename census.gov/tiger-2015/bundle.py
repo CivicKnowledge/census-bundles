734537f14aba939a9db7d1464b3ca8112f0243d8
Eric Busboom <eric@clarinova.com>
2016-01-30 07:30:03 -0800

    .

---
+++ census.gov/tiger-2015/bundle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import ambry.bundle
+
+
+class Bundle(ambry.bundle.Bundle):
+    pass
+    
+    def parse_state(self, v):
+        pass
+
---
+++ census.gov/tiger-2015/documentation.md
@@ -0,0 +1,2 @@
+
+US Census Tiger shape files have an SRID of [EPSG:4269.](http://spatialreference.org/ref/epsg/nad83/)
